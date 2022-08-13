e4d3c23bc31337e2aab0b58ffaabae12ba8cfe4b
Eugene Kosuhin <kosuha606@gmail.com>
2022-08-13 21:14:28 +0300

    feat: app basic routing

---
+++ src/Components/Contexts/Menu/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="menu screen">
+        <a class="menu_item" @click.prevent="$emit('open', 'start')" href="#">
+            Начать игру
+        </a>
+        <a class="menu_item" @click.prevent="$emit('open', 'settings')" href="#">
+            Настройки
+        </a>
+    </div>
+</template>
+
+<style scoped>
+    .menu_item {
+        cursor: pointer;
+    }
+</style>
---
+++ src/Components/Contexts/Tetris/Tetris.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="tetris">
+        <TetrisGame
+            v-if="screen==='start'"
+            @back="back"
+        />
+        <Settings
+            v-else-if="screen==='settings'"
+            @back="back"
+        />
+        <Menu v-else @open="open" />
+    </div>
+</template>
+
+<script setup>
+import Menu from "@/src/Components/Contexts/Menu/Menu";
+import Settings from "@/src/Components/Contexts/Settings/Settings";
+import TetrisGame from "~/src/Components/Contexts/Tetris/TetrisGame";
+
+const screen = ref('');
+
+const open = (name) => {
+    screen.value = name;
+}
+
+const back = () => {
+    screen.value = '';
+}
+</script>
+
+<style scoped>
+    .tetris {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 300px;
+        height: 400px;
+        border: solid 1px #333;
+        background: #fff;
+    }
+</style>
