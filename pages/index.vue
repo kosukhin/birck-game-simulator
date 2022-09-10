001bf88673d8a376f74539a84601834a29a03783
Eugene Kosukhin <githubkosuhin@gmail.com>
2022-09-10 19:05:18 +0300

    fix: link

--- pages/index.vue
+++ pages/index.vue
@@ -29,9 +29,11 @@
                             'Perhaps in the future the project will grow into something more than a work for a portfolio. You can help the author in this difficult task and create your own game based on this simulator. You will find a detailed tutorial on how to make your own game here:'
                         )
                     }}
-                    <el-link href="/new-game/" type="primary">{{
-                        $services.lang.t('Create new game')
-                    }}</el-link>
+                    <nuxt-link to="/new-game/">
+                        <el-link type="primary">{{
+                            $services.lang.t('Create new game')
+                        }}</el-link>
+                    </nuxt-link>
                 </p>
             </div>
         </div>
