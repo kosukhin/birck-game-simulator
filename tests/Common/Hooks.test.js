3448263d67ba190952124b288f8e41b6579e3b0b
Eugene Kosukhin <githubkosuhin@gmail.com>
2022-09-13 15:23:59 +0300

    feat: hooks test

---
+++ tests/Common/Hooks.test.js
@@ -0,0 +1,9 @@
+import services from '~~/src/Injections/Services'
+
+test('tetris service', () => {
+    const gamesList = {}
+    services.hooks.gamesResolving.runSubscribers(gamesList)
+    expect(gamesList).toHaveProperty('tetris')
+    expect(gamesList).toHaveProperty('tanks')
+    expect(gamesList).toHaveProperty('snake')
+})
