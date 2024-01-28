49a6ea9243ac0be430ec495f176001efd8040746
Eugene Kosukhin <kosuha606@gmail.com>
2024-01-28 10:08:06 +0300

    feat: Переделать змейку полностью

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './src/**/*.{js,vue,ts}',
+    './pages/**/*.{js,vue,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
