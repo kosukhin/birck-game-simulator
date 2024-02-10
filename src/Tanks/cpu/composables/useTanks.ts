cfd4cb7687f93953ff85419f374e0c9ac711f1de
Eugene Kosukhin <kosuha606@gmail.com>
2024-02-10 22:25:38 +0300

    feat: tanks

--- src/Tanks/cpu/composables/useTanks.ts
+++ src/Tanks/cpu/composables/useTanks.ts
@@ -1,4 +1,6 @@
+import { uniqueId } from 'lodash'
 import { EMoveDirection } from './../../../Common/Types/GameTypes'
+import { Block } from '~~/src/Common/cpu/providers/types/Block'
 import { GameGrid, GameSettings } from '~~/src/Common/cpu/providers/types/Game'
 import { FType, State } from '~~/src/Common/cpu/utils/system'
 
@@ -10,9 +12,13 @@ export const useTanks = (
   const gameSettings = getGameSettings()
   const gameGrid = getGameGrid()
 
+  gameGrid.get().blocks.push(...tankScheme)
+
   const nextFrame = () => {
     doTimer(gameSettings.get().speed, () => {
       gameSettings.get().frameCounter += 1
+      console.log('next frame')
+      tanksMainCycle(gameGrid.get())
       !gameSettings.get().isGameOver &&
         !gameSettings.get().isPaused &&
         nextFrame()
@@ -36,3 +42,14 @@ export const useTanks = (
     },
   }
 }
+
+const tanksMainCycle = (gameGrid: GameGrid) => {}
+
+const tankScheme: Block[] = [
+  { x: 0, y: 0, id: uniqueId('tank_'), group: 'tank' },
+  { x: 1, y: 0, id: uniqueId('tank_'), group: 'tank' },
+  { x: 1, y: 1, id: uniqueId('tank_'), group: 'tank' },
+  { x: 2, y: 1, id: uniqueId('tank_'), group: 'tank' },
+  { x: 0, y: 2, id: uniqueId('tank_'), group: 'tank' },
+  { x: 1, y: 2, id: uniqueId('tank_'), group: 'tank' },
+]
